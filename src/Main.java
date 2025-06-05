1344dcb2debde29b0bff66df6f7f98dbbc69cdff
b_oss <aa.vlzjr.23@gmail.com>
2025-06-05 12:54:17 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//import pages.LoginPage;
+import pages.ModifiedVersion;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ModifiedVersion::new);
+    }
+}
+
+/*
+    Already in the database:
+    admin | 1234
+    ariel | 1234
+*/
